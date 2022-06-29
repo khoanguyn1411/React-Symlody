18c8a3ff8e7cc1cb802c153fcbe3e2610a7cf6b2
khoanguyn1411 <khoaah1411@gmail.com>
2022-06-29 21:19:26 +0700

    [CLUB 104] Add UI header

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -9,11 +9,38 @@ export const Header: React.FC<TProps> = ({ className }) => {
   return (
     <header
       className={classNames(
-        "flex items-center h-header sticky top-0 ml-0 xl:ml-sidebar text-white bg-primary-800",
+        "flex items-center justify-between px-4 h-header sticky top-0 ml-0 xl:ml-sidebar text-white bg-primary-800",
         className
       )}
     >
-      <h1 className="font-bold">HEADER</h1>
+      <div className="relative w-2/3 mr-5">
+        <input
+          className="w-full px-2 py-1 text-black outline-none rounded-md"
+          placeholder="Tìm kiếm tài sản, sự kiện,..."
+        />
+        <span className="absolute top-0 bottom-0 flex items-center text-sm text-black cursor-pointer right-3">
+          <i className="fas fa-search" />
+        </span>
+      </div>
+
+      <div className="flex items-center justify-end flex-1 min-w-max">
+        <span className="text-xl text-white cursor-pointer mr-7">
+          <i className="fas fa-bell" />
+        </span>
+
+        <div className="flex items-center">
+          <div className="mr-4 bg-green-300 rounded-full w-9 h-9"></div>
+          <div className="flex flex-col mb-[2px]">
+            <div className="flex cursor-pointer">
+              <h1 className="font-bold">Đặng Khánh Linh</h1>
+              <span className="ml-4">
+                <i className="fas fa-caret-down" />
+              </span>
+            </div>
+            <h1 className="text-xs">Thành viên ban ABC</h1>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
