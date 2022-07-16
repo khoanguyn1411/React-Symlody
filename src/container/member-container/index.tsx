902d21ea5467dbbe5662ad5473e20bf7210071d3
khoanguyn1411 <khoaah1411@gmail.com>
2022-07-16 18:43:32 +0700

    [CLUB 102] Add tbody to table

--- src/container/member-container/index.tsx
+++ src/container/member-container/index.tsx
@@ -11,7 +11,7 @@ export const MemberContainer: React.FC = () => {
   return (
     <div>
       <div className="flex items-center justify-between py-3 bg-white border-b border-gray-200 px-default">
-        <h1>Quản lý thành viên</h1>
+        <h1>Quản lý tdành viên</h1>
         <div className="flex items-center">
           <div className="mr-5">
             <Switch></Switch>
@@ -28,59 +28,63 @@ export const MemberContainer: React.FC = () => {
       <div className="p-default">
         <div className="mt-3 bg-white border border-gray-200 rounded-lg">
           <table className="w-full">
-            <tr>
-              <th className="w-2/5 py-2 font-semibold text-left pl-18">
-                Họ và tên
-              </th>
-              <th className="w-1/5 py-2 font-semibold text-left">Ban</th>
-              <th className="w-1/5 py-2 font-semibold text-left">Ngày sinh</th>
-              <th className="w-1/5 py-2 font-semibold text-left">Vị trí</th>
-            </tr>
+            <tbody>
+              <tr>
+                <td className="w-2/5 py-2 font-semibold text-left pl-18">
+                  Họ và tên
+                </td>
+                <td className="w-1/5 py-2 font-semibold text-left">Ban</td>
+                <td className="w-1/5 py-2 font-semibold text-left">
+                  Ngày sinh
+                </td>
+                <td className="w-1/5 py-2 font-semibold text-left">Vị trí</td>
+              </tr>
 
-            <tr className="text-left border-t border-gray-200">
-              <th className="w-2/5 py-2 font-normal px-default">
-                <div className="flex items-center">
-                  <div className="w-10 h-10 mr-3 rounded-full bg-primary-800"></div>
-                  <div>
-                    <h1 className="font-semibold">Anh Khoa</h1>
-                    <h1 className="text-sm">anhkhoa@gmail.com</h1>
+              <tr className="text-left border-t border-gray-200">
+                <td className="w-2/5 py-2 font-normal px-default">
+                  <div className="flex items-center">
+                    <div className="w-10 h-10 mr-3 rounded-full bg-primary-800"></div>
+                    <div>
+                      <h1 className="font-semibold">Anh Khoa</h1>
+                      <h1 className="text-sm">anhkhoa@gmail.com</h1>
+                    </div>
                   </div>
-                </div>
-              </th>
-              <th className="w-1/5 py-2 font-normal">Sự kiện</th>
-              <th className="w-1/5 py-2 font-normal">11/01/1999</th>
-              <th className="w-1/5 py-2 font-normal">Leader</th>
-            </tr>
+                </td>
+                <td className="w-1/5 py-2 font-normal">Sự kiện</td>
+                <td className="w-1/5 py-2 font-normal">11/01/1999</td>
+                <td className="w-1/5 py-2 font-normal">Leader</td>
+              </tr>
 
-            <tr className="text-left border-t border-gray-200">
-              <th className="w-2/5 py-2 font-normal px-default">
-                <div className="flex items-center">
-                  <div className="w-10 h-10 mr-3 rounded-full bg-primary-800"></div>
-                  <div>
-                    <h1 className="font-semibold">Anh Khoa</h1>
-                    <h1 className="text-sm">anhkhoa@gmail.com</h1>
+              <tr className="text-left border-t border-gray-200">
+                <td className="w-2/5 py-2 font-normal px-default">
+                  <div className="flex items-center">
+                    <div className="w-10 h-10 mr-3 rounded-full bg-primary-800"></div>
+                    <div>
+                      <h1 className="font-semibold">Anh Khoa</h1>
+                      <h1 className="text-sm">anhkhoa@gmail.com</h1>
+                    </div>
                   </div>
-                </div>
-              </th>
-              <th className="w-1/5 py-2 font-normal">Sự kiện</th>
-              <th className="w-1/5 py-2 font-normal">11/01/1999</th>
-              <th className="w-1/5 py-2 font-normal">Leader</th>
-            </tr>
+                </td>
+                <td className="w-1/5 py-2 font-normal">Sự kiện</td>
+                <td className="w-1/5 py-2 font-normal">11/01/1999</td>
+                <td className="w-1/5 py-2 font-normal">Leader</td>
+              </tr>
 
-            <tr className="text-left border-t border-gray-200">
-              <th className="w-2/5 py-2 font-normal px-default">
-                <div className="flex items-center">
-                  <div className="w-10 h-10 mr-3 rounded-full bg-primary-800"></div>
-                  <div>
-                    <h1 className="font-semibold">Anh Khoa</h1>
-                    <h1 className="text-sm">anhkhoa@gmail.com</h1>
+              <tr className="text-left border-t border-gray-200">
+                <td className="w-2/5 py-2 font-normal px-default">
+                  <div className="flex items-center">
+                    <div className="w-10 h-10 mr-3 rounded-full bg-primary-800"></div>
+                    <div>
+                      <h1 className="font-semibold">Anh Khoa</h1>
+                      <h1 className="text-sm">anhkhoa@gmail.com</h1>
+                    </div>
                   </div>
-                </div>
-              </th>
-              <th className="w-1/5 py-2 font-normal">Sự kiện</th>
-              <th className="w-1/5 py-2 font-normal">11/01/1999</th>
-              <th className="w-1/5 py-2 font-normal">Leader</th>
-            </tr>
+                </td>
+                <td className="w-1/5 py-2 font-normal">Sự kiện</td>
+                <td className="w-1/5 py-2 font-normal">11/01/1999</td>
+                <td className="w-1/5 py-2 font-normal">Leader</td>
+              </tr>
+            </tbody>
           </table>
         </div>
       </div>
