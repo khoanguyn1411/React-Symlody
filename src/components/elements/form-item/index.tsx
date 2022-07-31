77ae02caa2d7de3845838680db86d18295da40b1
khoanguyn1411 <khoaah1411@gmail.com>
2022-07-31 21:14:56 +0700

    [CLUB 150] Add filter selection

--- src/components/elements/form-item/index.tsx
+++ src/components/elements/form-item/index.tsx
@@ -33,7 +33,7 @@ export const FormItem: React.FC<IFormItemProps> = ({
         {label && (
           <label
             htmlFor="title"
-            className="block font-medium text-black line-height-normal"
+            className="block mb-2 font-medium text-black line-height-normal"
           >
             {label}{" "}
             {isRequired ? <span className="text-red-400">*</span> : null}
