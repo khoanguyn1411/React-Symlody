1baeb40710b51395ed24dd525e5fe2af9dca418e
khoanguyn1411 <khoaah1411@gmail.com>
2022-08-05 00:09:24 +0700

    [CLUB 150] Remove redundant import

--- src/features/reducers/member-reducer/index.ts
+++ src/features/reducers/member-reducer/index.ts
@@ -1,7 +1,6 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 import {
-  Api,
   MemberApi,
   RequestCreateMembersResult,
   RequestGetMembersResult,
