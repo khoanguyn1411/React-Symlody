9c2942827951a912c6826bd195876e25c66c0cf1
khoanguyn1411 <khoaah1411@gmail.com>
2022-08-16 01:07:34 +0700

    [CLUB 151] Add return type for useEffectSkipFirstRender hook

--- src/hooks/useEffectSkipFirstRender.ts
+++ src/hooks/useEffectSkipFirstRender.ts
@@ -3,7 +3,7 @@ import { DependencyList, useEffect, useRef } from "react";
 export const useEffectSkipFirstRender = (
   callback: () => void,
   deps: DependencyList
-) => {
+): void => {
   const isFirstMounted = useRef(false);
   useEffect(() => {
     if (!isFirstMounted.current) {
@@ -13,6 +13,4 @@ export const useEffectSkipFirstRender = (
     callback && callback();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, deps);
-
-  return {};
 };
