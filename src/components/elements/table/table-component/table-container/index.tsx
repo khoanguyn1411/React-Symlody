7190dfa8b42b51cefb993a2953a00c1405f0c4a5
khoanguyn1411 <khoaah1411@gmail.com>
2022-09-03 22:09:20 +0700

    [CLUB 198] Remove top border of table

--- src/components/elements/table/table-component/table-container/index.tsx
+++ src/components/elements/table/table-component/table-container/index.tsx
@@ -3,7 +3,7 @@ import { GlobalTypes } from "@/global";
 export const TableContainer: GlobalTypes.FCChildren = ({ children }) => {
   return (
     <div className="h-table">
-      <div className="border rounded-md">
+      <div className="border border-t-0 rounded-md">
         <div className="overflow-auto max-h-table rounded-md">
           <div className="bg-white border-gray-200 rounded-t-lg rounded-md">
             <table className="w-full border-separate border-spacing-0">
