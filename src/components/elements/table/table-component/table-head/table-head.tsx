5948a5b834a772608e8f3b367bb0cd4f6fb0cef5
Huynh Nhat Hao <haohuynh11011999@gmail.com>
2022-09-19 17:41:55 +0700

    update sidebar mobile

--- src/components/elements/table/table-component/table-head/table-head.tsx
+++ src/components/elements/table/table-component/table-head/table-head.tsx
@@ -2,7 +2,7 @@ import { GlobalTypes } from "@/global";
 
 export const TableHead: GlobalTypes.FCChildren = ({ children }) => {
   return (
-    <thead className="sticky top-0 bg-primary-50">
+    <thead className="sticky top-0 z-[1] bg-primary-50">
       <tr>{children}</tr>
     </thead>
   );
