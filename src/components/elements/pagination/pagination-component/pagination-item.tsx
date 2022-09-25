508f7ee93c74d870f8ab0c585705c29cbeeb7ee0
khoanguyn1411 <khoaah1411@gmail.com>
2022-09-25 17:59:58 +0700

    [CLUB 225] Update transition of pagination:

--- src/components/elements/pagination/pagination-component/pagination-item.tsx
+++ src/components/elements/pagination/pagination-component/pagination-item.tsx
@@ -19,7 +19,7 @@ const _PaginationItem: React.FC<TProps> = ({ pageIndex }) => {
     <button
       onClick={handleChangeActivePage}
       className={classNames(
-        "min-w-[32px] px-2 transition-all duration-75 hover:bg-primary-50 h-8 flex items-center justify-center cursor-pointer",
+        "min-w-[32px] px-2 hover:bg-primary-50 hover:transition-colors hover:duration-200 h-8 flex items-center justify-center cursor-pointer",
         {
           "bg-primary-800 text-white hover:bg-primary-800 font-semibold":
             activePage === pageIndex,
