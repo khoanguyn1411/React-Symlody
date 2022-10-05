96921be336d161e868ebd29475ab2e84adae4c61
Hao Huynh <haohuynh11011999@gmail.com>
2022-10-05 20:45:39 +0700

    [ci/cd] setup dockerfile

--- src/api/api-config.ts
+++ src/api/api-config.ts
@@ -1,4 +1,1 @@
-export const API_URL =
-  process.env.REACT_APP_IS_PRODUCT === "true"
-    ? (process.env.REACT_APP_WEB_API as string)
-    : (process.env.REACT_APP_WEB_API_LOCAL as string);
+export const API_URL = process.env.REACT_APP_WEB_API as string;
