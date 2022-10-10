2d5d1faa1b36e1584c4549a0f954f795861ac981
khoanguyn1411 <khoaah1411@gmail.com>
2022-10-10 23:12:54 +0700

    [CLUB 236] Remove redundant function in filter service

--- src/utils/services/filter-service.ts
+++ src/utils/services/filter-service.ts
@@ -8,15 +8,3 @@ import { toCleanedString } from "./format-service";
 export function isTextIncludedIn(text: string, includedText: string): boolean {
   return toCleanedString(text).includes(toCleanedString(includedText));
 }
-
-/**
- * Check if element of an array is in another array.
- * @param arr1 Array 1.
- * @param arr2 Array 2.
- */
-export function isElementOfAnArrayInAnotherArray(
-  arr1: any[],
-  arr2: any[]
-): boolean {
-  return arr1.every((element) => arr2.includes(element));
-}
